cdfd8ea7d80091cc2baf6c25faeb18f7df5ec894
Angel-2002 <u202122519@upc.edu.pe>
2024-10-22 02:23:42 -0500

    fix: controller reservation

--- routes/Controller_reservation.py
+++ routes/Controller_reservation.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 from config.db_dependency import db_dependency
+from datetime import datetime
 #importar las clase
 from models.reservation import Reservation
 from schemas.reservation import ReservationS
@@ -8,7 +9,20 @@ reservation=APIRouter()
 
 @reservation.post("/reservation/", status_code=status.HTTP_201_CREATED, tags=["Reservation"])
 async def crear_reservation(reservation:Reservation, db:db_dependency):
-    db_reservation = ReservationS(**reservation.dict())
+    # Convertir el string a un objeto datetime
+    check_in_date = datetime.strptime(reservation.check_in_date, "%m/%d/%Y %I:%M %p")
+    check_out_date = datetime.strptime(reservation.check_out_date, "%m/%d/%Y %I:%M %p")
+    
+    db_reservation = ReservationS(
+        total_price=reservation.total_price,
+        stay_hours=reservation.stay_hours,
+        check_in_date=check_in_date,
+        check_out_date=check_out_date,
+        payment_method=reservation.payment_method,
+        post_id=reservation.post_id,
+        user_id=reservation.user_id
+    )
+    
     db.add(db_reservation)
     db.commit()
     return {"message": "Reservacion creada correctamente"}
